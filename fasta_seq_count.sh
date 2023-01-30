38d5798a52d52d462320396ad3e36bf4766b87ff
jakob sell <118688542+thatguyoverthere3@users.noreply.github.com>
2023-01-29 23:44:42 -0500

    Add files via upload
    Assignment 1

---
+++ fasta_seq_count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This script can count the number of sequences in a fasta file"
+greeting="Hello"
+name="Jakob"
+echo $greeting $name
+mkdir learning_bash
+cd learning_bash
+mkdir bioinformatics
+cp /home/jakobsell0/transcriptome.fasta /home/jakobsell0/learning_bash/bioinformatics
+grep -c "XM" /home/jakobsell0/learning_bash/bioinformatics/transcriptome.fasta>>/home/jakobsell0/learning_bash/bioinformatics/transcriptome_count.txt
